d11780542b233ca1e108a1f616cff945bb7fcdee
Warrick Holfeld <warrick87@gmail.com>
2020-05-13 15:26:42 -0700

    unit 4 done

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,61 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Screen max size
+@8191
+D=A
+@screenSize
+M=D
+
+// create temp variable to check if pressed key matches current temp if
+// don't match move to loop also set fill color in another temp location
+(KEYLOOP)
+@lastKey
+D=A
+@KBD
+
+//check if keys don't match and jump out
+D=D-M
+@FILLSCREEN
+D;JNE
+@KEYLOOP
+0;JMP
+
+// run fill loop
+(FILLSCREEN)
+@i
+M=0
+@color
+M=0
+@KBD
+D=M
+@PAINT
+D;JEQ
+@color
+M=-1
+
+(PAINT)
+@i
+D=M
+@SCREEN
+D=D+A
+@currentFill
+M=D
+@color
+D=M
+@currentFill
+A=M
+M=D
+
+//If iterator greater than screensize break loop
+@i
+M=M+1
+D=M
+@screenSize
+D=D-M
+@PAINT
+D;JLE
+
+@KEYLOOP
+0;JMP

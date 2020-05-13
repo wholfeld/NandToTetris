39479d02f09c6cdcd31abf8274479dd692d3a142
Warrick Holfeld <warrick87@gmail.com>
2020-05-12 22:57:22 -0700

    mulitplication working

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// Zero out R2
+@R2
+M=D
+
+(LOOP)
+// Check if multiplier is 0
+@R1
+D=M
+// If 0 jump to end
+@END
+D;JEQ
+// Subract 1 from multiplier
+@R1
+M=D-1
+// Take multiplicand and add it to R2
+@R0
+D=M
+@R2
+M=D+M
+// Repeat
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
